fc894106b93408db81a13e74fbbd2ce09c804475
Keshav-Biyani <82154850+Keshav-Biyani@users.noreply.github.com>
2021-10-30 18:56:29 +0530

    Completed

--- ques1.cpp
+++ ques1.cpp
@@ -6,14 +6,14 @@ using namespace std;
 class book
 {
   char **title, **author;
-  float price;
-  int no_copie, stock, total_price;
+  float *price;
+  int no_copie, *stock, total_price;
 
 public:
-int size;
+  int size;
   void input_data(const char *aut, const char *tit, float p, int s, int i);
   book();
-  void search(const char *aut, const char *tit);
+  void search();
   void copies(void);
   void tot(void);
   void display(void);
@@ -25,7 +25,8 @@ book::book()
   int length = 0;
   author = new char *[size];
   title = new char *[size];
-  
+  price=new float[size];
+  stock=new int[size];
 }
 
 void book ::input_data(const char *aut, const char *tit, float p, int s, int i)
@@ -33,41 +34,74 @@ void book ::input_data(const char *aut, const char *tit, float p, int s, int i)
 
   {
     int length = strlen(aut);
-    // author = new char *[length + 1];
+    // author = new char *[length + 1];//
     author[i] = new char[length + 1];
     strcpy(author[i], aut);
   }
   {
     int length = strlen(tit);
-    // title = new char *[length + 1];
+    // title = new char *[length + 1];//
     title[i] = new char[length + 1];
     strcpy(title[i], tit);
   }
 
-  price = p;
-  stock = s;
-  // cout<<author[i]<<endl;
-  // cout<<title[i]<<endl;
+  price[i] = p;
+  stock[i]= s;
+  // cout<<author[i]<<endl;//
+  // cout<<title[i]<<endl;//
 }
 void book ::display()
 {
   cout << "Author" << setw(30) << "Title" << setw(30) << "Price" << setw(10) << "Stock" << endl;
   for (int i = 0; i < size; i++)
   {
-    cout << author[i] << setw(20) << title[i] << setw(30) << "Price" << setw(10) << "Stock" << endl;
+    cout << author[i] << setw(20) << title[i] << setw(30) << price[i] << setw(10) << stock[i] << endl;
   }
 }
 
-// void book ::search(const char *aut, const char *tit)
-// {
-// }
+void book ::search()
+{
+    char name[80];
+char t[80];
+
+
+  cout << "\nEnter name of author" << endl;
+  cin.ignore();
+  cin.getline(name, sizeof(name));
+  cout << "Enter title of book" << endl;
+  cin.getline(t, sizeof(t));
+  int a, b;
+  for (int i = 0; i < size; i++)
+  {
+    a = strcmp(name, author[i]);
+    b = strcmp(t, title[i]);
+    if (a == 0 && b == 0)
+    {
+      cout << "Book is available" << endl;
+      cout<< "How much do you want to order"<<endl;
+      cin>>no_copie;
+
+      if ( stock[i]-no_copie<0)
+      {
+        cout<<"copies are not available"<<endl;
+
+
+      }else{
+        stock[i]=stock[i]-no_copie;
+        total_price=price[i]*no_copie;
+        cout<<"Total price will be = "<<total_price<<endl;
+      }
+
+    }
+  }
+}
 int main()
 {
   book b;
-  char *name;
-  name = new char[80];
-  char *t;
-  t = new char[80];
+  char name[80];
+char t[80];
+
+
   int s;
   float p;
   for (int i = 0; i < b.size; i++)
@@ -84,25 +118,25 @@ int main()
     b.input_data(name, t, p, s, i);
   }
   int choice;
-    while ( choice != 4)
-    {
-      
+  while (choice != 3)
+  {
 
-        
     cout << "1.for displaying all stoke" << endl;
-    cout << "2. for adding new books in stook" << endl;
-    cout << "3.Display account details" << endl;
-    cout << "4.Quit" << endl;
+    cout << "2. for coustomers" << endl;
+    cout << "3.Quit" << endl;
     cout << "Enter your choice: ";
     cin >> choice;
 
+    switch (choice)
+    {
+    case 1:
+      b.display();
+      break;
 
-        switch (choice)
-        {
-        case 1:
-         b.display();
-         break;
-         case 2:
+    case 2:
+    {
+      b.search();
+    }
+    }
+  }
 }
-
-    }}
